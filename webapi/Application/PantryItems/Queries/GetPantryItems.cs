7da2bd7fb24d23a3b5a566fc20330b1aa98a181f
Brendan Baker <78634345+TheMightyYak1@users.noreply.github.com>
2024-08-08 21:26:42 +1000

    getPantryItems query built

--- webapi/Application/PantryItems/Queries/GetPantryItems.cs
+++ webapi/Application/PantryItems/Queries/GetPantryItems.cs
@@ -2,29 +2,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Common.Interfaces;
 using Application.Core;
+using Domain.Entities;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.PantryItems.Queries;
 public class GetPantryItems
 {
-    public class Query : IRequest<Result<Unit>>
+    public class Query : IRequest<Result<List<PantryItem>>>
     {
         
     }
 
-    public class Handler : IRequestHandler<Query, Result<Unit>>
+    public class Handler : IRequestHandler<Query, Result<List<PantryItem>>>
     {
-        // private readonly DbContent
+        private readonly IPantryDbContext _pantryDbContext;
 
-        public Handler()
+        public Handler (IPantryDbContext pantryDbContext)
         {
-        
+            _pantryDbContext = pantryDbContext;
         }
 
-        public async Task<Result<Unit>> Handle(Query request, CancellationToken cancellationToken)
+        public async Task<Result<List<PantryItem>>> Handle(Query request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var pantryItems = await _pantryDbContext.PantryItems
+                .ToListAsync(cancellationToken);
+
+            if (pantryItems == null) return Result<List<PantryItem>>.Failure("No Pantry Items");
+
+            return Result<List<PantryItem>>.Success(pantryItems);
         }
     }
 }   
